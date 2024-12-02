3de9fd2b288d18c8a69e33e2ceefb4cd618c4675
Denzelllll <denzelsun01@gmail.com>
2024-12-02 03:55:12 -0500

    1

---
+++ sketch_nov30a.ino
@@ -0,0 +1,47 @@
+const int potentiometerPin = A0; 
+const int button1Pin = 2;        
+const int button2Pin = 3;        
+const int led1Pin = 13;          
+const int led2Pin = 12;          
+
+void setup() {
+  pinMode(potentiometerPin, INPUT);
+  pinMode(button1Pin, INPUT_PULLUP);
+  pinMode(button2Pin, INPUT_PULLUP);
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int potValue = analogRead(potentiometerPin); 
+  bool button1Pressed = digitalRead(button1Pin) == LOW; 
+  bool button2Pressed = digitalRead(button2Pin) == LOW; 
+
+
+  if (potValue > 1000) {
+
+    if (button1Pressed && button2Pressed) {
+      digitalWrite(led1Pin, HIGH); 
+      digitalWrite(led2Pin, LOW);  
+    }
+
+    else if (button1Pressed || button2Pressed) {
+      digitalWrite(led1Pin, LOW);  
+      digitalWrite(led2Pin, HIGH);
+    }
+
+    else {
+      digitalWrite(led1Pin, LOW);
+      digitalWrite(led2Pin, LOW);
+
+    }
+  } else {
+  
+    digitalWrite(led1Pin, LOW);
+    digitalWrite(led2Pin, LOW);
+    Serial.println("Potentiometer not at maximum. Both LEDs are OFF.");
+  }
+
+  delay(200); 
+}
